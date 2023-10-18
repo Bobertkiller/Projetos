78a2260d61448d8e6314c8747251cbe3bd2d0f9d
Felipe M.B <felipe.mazzeo.barbosa@outlook.com>
2023-10-17 21:41:36 -0300

    Update AVLMain.java

--- Java/AVL/src/AVLMain.java
+++ Java/AVL/src/AVLMain.java
@@ -1,5 +1,5 @@
 import Avl.AVL;
-import Avl.Node; // Importe a classe Node
+import Avl.Node;
 
 public class AVLMain {
     public static void main(String[] args) {
@@ -11,11 +11,13 @@ public class AVLMain {
         avlTree.insert(2);
         avlTree.insert(3);
 
-        // Exibe os dados atualizados dos nós
+        // Exibe os dados atualizados dos nós após a inserção
         System.out.println("Dados dos nós após inserção:");
         printNodeData(avlTree.getroot());
 
-    
+        // O balanceamento ocorre automaticamente durante a inserção
+        // Portanto, não é necessário chamar explicitamente o balanceamento.
+
         System.out.println("\nDados dos nós após o balanceamento:");
         printNodeData(avlTree.getroot());
     }
@@ -24,10 +26,11 @@ public class AVLMain {
     private static void printNodeData(Node node) {
         if (node != null) {
             System.out.println("Chave: " + node.getdata());
-            System.out.println("Nó Pai: " + (node.getparent() != null ? node.getparent().getdata() : "null")) ;
-            System.out.println( "Nó Filho Esquerdo: " + (node.getleft() != null ? node.getleft().getdata() : "null"));
-            System.out.println( "Nó Filho Direito: " + (node.getright() != null ? node.getright().getdata() : "null"));
-            System.out.println( "Fator de Balanceamento: " + node.getBalanceFactor());
+            System.out.println("Nó Pai: " + (node.getparent() != null ? node.getparent().getdata() : "null"));
+            System.out.println("Nó Filho Esquerdo: " + (node.getleft() != null ? node.getleft().getdata() : "null"));
+            System.out.println("Nó Filho Direito: " + (node.getright() != null ? node.getright().getdata() : "null"));
+            System.out.println("Fator de Balanceamento: " + node.getBalanceFactor());
+            System.out.println("Altura: " + node.getHeight());
             System.out.println("--------");
 
             printNodeData(node.getleft());
