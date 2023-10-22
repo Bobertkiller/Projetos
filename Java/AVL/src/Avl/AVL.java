9db8e77eab0044107a30aded34cb9caaf36cf1cf
Felipe M.B <felipe.mazzeo.barbosa@outlook.com>
2023-10-22 11:50:52 -0300

    Update AVL.java

--- Java/AVL/src/Avl/AVL.java
+++ Java/AVL/src/Avl/AVL.java
@@ -5,71 +5,132 @@ package Avl;
 
 public class AVL extends BT {
 
-    public AVL() {
-        super();
+  public AVL() {
+    super();
+  }
+
+  private Node rotateLeft(Node root) {
+    Node newRoot = root.getright();
+    root.setright(newRoot.getleft());
+    newRoot.setleft(root);
+    root.updateBalanceFactor();
+    newRoot.updateBalanceFactor();
+    return newRoot;
+  }
+
+  private Node rotateRight(Node root) {
+    Node newRoot = root.getleft();
+    root.setleft(newRoot.getright());
+    newRoot.setright(root);
+    root.updateBalanceFactor();
+    newRoot.updateBalanceFactor();
+    return newRoot;
+  }
+
+  private Node rotateLeftRight(Node root) {
+    root.setleft(rotateLeft(root.getleft()));
+    return rotateRight(root);
+  }
+
+  private Node rotateRightLeft(Node root) {
+    root.setright(rotateRight(root.getright()));
+    return rotateLeft(root);
+  }
+
+  public void insert(int chave) {
+    super.insert(chave);
+    root = balance(root);
+  }
+
+  private Node balance(Node node) {
+    if (node == null) {
+      return node;
     }
 
-    // Rotação simples para a esquerda (LL)
-    private Node rotateLeft(Node root) {
-        Node newRoot = root.getright();
-        root.setright(newRoot.getleft());
-        newRoot.setleft(root);
-        root.updateBalanceFactor();
-        newRoot.updateBalanceFactor();
-        return newRoot;
+    node.updateBalanceFactor();
+
+    if (node.getBalanceFactor() > 1) {
+      if (node.getleft().getBalanceFactor() >= 0) {
+        return rotateRight(node);
+      } else {
+        return rotateLeftRight(node);
+      }
+    } else if (node.getBalanceFactor() < -1) {
+      if (node.getright().getBalanceFactor() <= 0) {
+        return rotateLeft(node);
+      } else {
+        return rotateRightLeft(node);
+      }
     }
 
-    // Rotação simples para a direita (RR)
-    private Node rotateRight(Node root) {
-        Node newRoot = root.getleft();
-        root.setleft(newRoot.getright());
-        newRoot.setright(root);
-        root.updateBalanceFactor();
-        newRoot.updateBalanceFactor();
-        return newRoot;
+    return node;
+  }
+
+  public void remove(int chave) {
+    root = remove(root, chave);
+  }
+
+  private Node remove(Node node, int chave) {
+    if (node == null) {
+      return node;
     }
 
-    // Rotação esquerda-direita (LR)
-    private Node rotateLeftRight(Node root) {
-        root.setleft(rotateLeft(root.getleft()));
-        return rotateRight(root);
-    }
+    if (chave < node.getdata()) {
+      node.setleft(remove(node.getleft(), chave));
+    } else if (chave > node.getdata()) {
+      node.setright(remove(node.getright(), chave));
+    } else {
+      // Nó a ser removido encontrado
 
-    // Rotação direita-esquerda (RL)
-    private Node rotateRightLeft(Node root) {
-        root.setright(rotateRight(root.getright()));
-        return rotateLeft(root);
-    }
+      if (node.getleft() == null || node.getright() == null) {
+        Node temp = (node.getleft() != null) ? node.getleft() : node.getright();
 
-    // Inserir um nó na árvore AVL (chamando o método da classe base)
-    public void insert(int chave) {
-        super.insert(chave);
-        root = balance(root);
-    }
-
-    // Método auxiliar para reequilibrar a árvore após a inserção
-    private Node balance(Node node) {
-        if (node == null) {
-            return node;
+        if (temp == null) {
+          // Nó é uma folha ou tem um único filho
+          temp = node;
+          node = null;
+        } else {
+          // Nó tem um único filho
+          node = temp;
         }
-
-        node.updateBalanceFactor();
-
-        if (node.getBalanceFactor() > 1) {
-            if (node.getleft().getBalanceFactor() >= 0) {
-                return rotateRight(node); // Rotação RR
-            } else {
-                return rotateLeftRight(node); // Rotação LR
-            }
-        } else if (node.getBalanceFactor() < -1) {
-            if (node.getright().getBalanceFactor() <= 0) {
-                return rotateLeft(node); // Rotação LL
-            } else {
-                return rotateRightLeft(node); // Rotação RL
-            }
-        }
-
-        return node;
+      } else {
+        // Nó tem dois filhos
+        Node temp = findMin(node.getright()); // Encontre o nó mínimo na subárvore direita
+        node.setdata(temp.getdata()); // Copie o valor do nó mínimo
+        node.setright(remove(node.getright(), temp.getdata())); // Remova o nó mínimo
+      }
     }
+
+    if (node == null) {
+      return node;
+    }
+
+    node.updateBalanceFactor();
+
+    // Balanceie a árvore após a remoção
+    if (node.getBalanceFactor() > 1) {
+      if (node.getleft().getBalanceFactor() >= 0) {
+        return rotateRight(node); // Rotação RR
+      } else {
+        return rotateLeftRight(node); // Rotação LR
+      }
+    } else if (node.getBalanceFactor() < -1) {
+      if (node.getright().getBalanceFactor() <= 0) {
+        return rotateLeft(node); // Rotação LL
+      } else {
+        return rotateRightLeft(node); // Rotação RL
+      }
+    }
+
+    return node;
+  }
+
+  private Node findMin(Node node) {
+    while (node.getleft() != null) {
+      node = node.getleft();
+    }
+    return node;
+  }
 
 }
+
